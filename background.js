3e9db5c4fa646bc8548cf74c133f2c549df89426
Kryštof Řezáč <krystofrezac@gmail.com>
2025-05-27 08:09:37 +0200

    Fix start page closing

--- background.js
+++ background.js
@@ -1,20 +1,39 @@
 let openedNewWindow = false
 
+const closeStartPages = ()=>{
+  chrome.tabs.query({}, allPages => {
+    if (allPages.length > 1) {
+      const startPages = allPages.filter(page => page.url.startsWith("chrome://vivaldi-webui/startpage"))
+      console.log("Closed", startPages)
+      chrome.tabs.remove(startPages.map(startPage => startPage.id))
+    }
+  })
+}
+
 chrome.tabs.onCreated.addListener((tab) => {
   console.log("Intercepted", tab, openedNewWindow)
+
+  closeStartPages()
+
   const url = tab.pendingUrl ?? tab.url;
 
+  if (openedNewWindow) {
+    console.log("Just opened the tab, ignoring")
+    openedNewWindow = false;
+    return;
+  }
+
   if (!url) {
-    console.log("No url")
+    console.log("No url, creating window and moving the tab there")
+    chrome.windows.create({}, (window)=>{
+      console.log("created window")
+      chrome.tabs.move(tab.id, {windowId: window.id, index: 0})
+      closeStartPages()
+    })
     return;
   }
   if (url.startsWith("chrome://newtab")) {
     console.log("chrome://newtab")
-    return;
-  }
-  if (openedNewWindow) {
-    console.log("Just opened the tab, ignoring")
-    openedNewWindow = false;
     return;
   }
 
